ef63e43afd6d65a8b4fb9959a0bbbbe75498ebbc
tshemsedinov <timur.shemsedinov@gmail.com>
2018-05-05 03:27:46 +0300

    Implement API for imperative tests

--- test/imperative.js
+++ test/imperative.js
@@ -2,7 +2,22 @@
 
 const mt = require('..');
 
-mt.test('Imperative example', (test) => {
+mt.test('strictSame', (test) => {
   test.strictSame(1, 1);
+  test.end();
+});
+
+mt.test('assert', (test) => {
+  test.assert(1);
+  test.end();
+});
+
+mt.test('assertNot', (test) => {
+  test.assertNot(1);
+  test.end();
+});
+
+mt.test('same', (test) => {
+  test.same(1, 1);
   test.end();
 });
