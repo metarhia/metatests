48055de0bbb4de2a2b617e30f6851b429c666e67
tshemsedinov <timur.shemsedinov@gmail.com>
2018-05-16 01:31:30 +0300

    Implement .type() and .error() methods
    PR-URL: https://github.com/metarhia/metatests/pull/44

--- test/imperative.js
+++ test/imperative.js
@@ -19,3 +19,14 @@ metatests.test('same', (test) => {
   test.same(1, '1');
   test.end();
 });
+
+metatests.test('error', (test) => {
+  test.error({}, 'that\'s ok');
+  test.end();
+});
+
+metatests.test('type', (test) => {
+  test.type(new Date(), 'Date');
+  test.type(new Date(), 'object');
+  test.end();
+});
