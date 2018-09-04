38a2c700219c1b8db711c238732013e5d6d91028
Denys Otrishko <shishugi@gmail.com>
2018-09-04 16:17:41 +0300

    Remove unnecessary exit-code test
    This test in case of failure would still return 'success'
    because if metatests has failed it would exit with code -1, not
    necessary because the test we want has failed.
    
    PR-URL: https://github.com/metarhia/metatests/pull/81

--- test/assets.js
+++ test/assets.js
@@ -1,14 +1,7 @@
 'use strict';
 
 const { ImperativeTest } = require('..');
+
 const test = new ImperativeTest('Slave test');
-
-const fail = process.argv[2] === 'f';
-
-if (fail) {
-  test.fail('Failing test');
-} else {
-  test.pass('Passing test');
-}
-
+test.pass('Passing test');
 test.end();
