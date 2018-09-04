d2c537f612487f7001cb50cb9080db3ef7e6cf53
Denys Otrishko <shishugi@gmail.com>
2018-09-04 16:17:41 +0300

    Remove unnecessary exit-code test
    This test in case of failure would still return 'success'
    because if metatests has failed it would exit with code -1, not
    necessary because the test we want has failed.

--- test/exit-code.js
+++ test/exit-code.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const cp = require('child_process');
-
 const path = require('path');
 
 const metatests = require('..');
@@ -13,21 +12,6 @@ metatests.test('exit code of a passing test must be 0', test => {
 
   subtest.on('exit', code => {
     test.equal(code, 0);
-    test.end();
-  });
-
-  subtest.on('error', error => {
-    test.fail(error);
-  });
-});
-
-metatests.test('exit code of a failing test must not be 0', test => {
-  const subtest = cp.fork(path.join(__dirname, 'assets'), ['f'], {
-    stdio: 'ignore',
-  });
-
-  subtest.on('exit', code => {
-    test.notEqual(code, 0);
     test.end();
   });
 
