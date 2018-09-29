6c641092261ab29f328bc81b58174438e2ee982a
Denys Otrishko <shishugi@gmail.com>
2018-09-29 14:39:09 +0300

    Implement simple function comparison via toString
    * compare 'function' with toString in equal and strictEqual
    * add special reported case to output them properly
    * add tests
    
    PR-URL: https://github.com/metarhia/metatests/pull/95

--- test/compare.js
+++ test/compare.js
@@ -70,6 +70,19 @@ assert(!equal([true, false], [true, true]));
 assert(equal([NaN, Infinity], [NaN, Infinity]));
 assert(!equal([NaN, Infinity], [Infinity, NaN]));
 
+assert(equal(() => {}, () => {}));
+assert(equal(() => 3, () => 3));
+assert(equal(a => a, a => a));
+assert(equal((a, b) => a + b, (a, b) => a + b));
+assert(equal(
+  function(a, b) { a += 3; return this.c + a + b; },
+  function(a, b) { a += 3; return this.c + a + b; }
+));
+
+const func = function(a) { return a; };
+assert(equal(func, func));
+assert(!equal(console.log, console.dir));
+
 assert(strictEqual(1, 1));
 assert(!strictEqual(1, 2));
 
@@ -135,3 +148,14 @@ assert(!strictEqual([true, false], [true, true]));
 
 assert(strictEqual([NaN, Infinity], [NaN, Infinity]));
 assert(!strictEqual([NaN, Infinity], [Infinity, NaN]));
+
+assert(strictEqual(() => {}, () => {}));
+assert(strictEqual(() => 3, () => 3));
+assert(strictEqual(a => a, a => a));
+assert(strictEqual((a, b) => a + b, (a, b) => a + b));
+assert(strictEqual(
+  function(a, b) { a += 3; return this.c + a + b; },
+  function(a, b) { a += 3; return this.c + a + b; }
+));
+assert(strictEqual(func, func));
+assert(!strictEqual(console.log, console.dir));
