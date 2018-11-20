b950f4007ae4fa900ec4bceae37d179ae5d43fde
Denys Otrishko <shishugi@gmail.com>
2018-11-20 12:33:30 +0200

    Add Runner.wait/resume to postpone 'finish' report
    PR-URL: https://github.com/metarhia/metatests/pull/122

--- test/runner.js
+++ test/runner.js
@@ -17,3 +17,51 @@ test('runner must not account todo as failure', test => {
     test.end();
   });
 });
+
+test('runner support wait/resume (resume before test end)', test => {
+  const runner = new Runner();
+  runner.removeReporter();
+  let resumeCalled = false;
+  runner.on('finish', () => {
+    test.strictSame(resumeCalled, true);
+    test.end();
+  });
+  runner.wait();
+  testSync('successful', () => {}, {}, runner);
+  process.nextTick(() => {
+    resumeCalled = true;
+    runner.resume();
+  });
+});
+
+test('runner support wait/resume (resume same tick with test end)', test => {
+  const runner = new Runner();
+  runner.removeReporter();
+  let resumeCalled = false;
+  runner.on('finish', () => {
+    test.strictSame(resumeCalled, true);
+    test.end();
+  });
+  runner.wait();
+  const t = testSync('successful', () => {}, {}, runner);
+  t.on('done', () => {
+    resumeCalled = true;
+    runner.resume();
+  });
+});
+
+test('runner support wait/resume (resume after test end)', test => {
+  const runner = new Runner();
+  runner.removeReporter();
+  let resumeCalled = false;
+  runner.on('finish', () => {
+    test.strictSame(resumeCalled, true);
+    test.end();
+  });
+  runner.wait();
+  const t = testSync('successful', () => {}, {}, runner);
+  t.on('done', () => setImmediate(() => {
+    resumeCalled = true;
+    runner.resume();
+  }));
+});
