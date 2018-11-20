9514ea65e8b43d2b66d9ffa8a57aa02c384b59c0
Mykola Bilochub <nbelochub@gmail.com>
2018-11-20 19:13:19 +0200

    Fix comparison in declarative tests
    Declarative tests compared actual and expected values by serializing
    them in different formats before comparison.
    
    Closes: https://github.com/metarhia/metatests/issues/123

--- test/declarative.js
+++ test/declarative.js
@@ -4,8 +4,9 @@ const metatests = require('..');
 
 const f1 = x => x * 2;
 const f2 = x => x + 3;
+const f3 = x => x;
 
-const namespace = { submodule: { f1, f2 } };
+const namespace = { submodule: { f1, f2, f3 } };
 
 metatests.case('Declarative example', namespace, {
   'submodule.f1': [
@@ -17,5 +18,8 @@ metatests.case('Declarative example', namespace, {
     [1, 4],
     [2, 5],
     [3, 6],
+  ],
+  'submodule.f3': [
+    ['\\', '\\'],
   ],
 });
