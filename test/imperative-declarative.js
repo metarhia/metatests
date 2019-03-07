fb459b1920166e3a9e6168e6dc89d3a21a6c7c3a
Denys Otrishko <shishugi@gmail.com>
2019-03-07 14:05:58 +0200

    Allow to run DeclarativeTest from ImperativeTest
    PR-URL: https://github.com/metarhia/metatests/pull/156

---
+++ test/imperative-declarative.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const { test, ImperativeTest } = require('..');
+
+const f1 = x => x * 2;
+
+const namespace = { f1 };
+
+test('nested successful case', test => {
+  let nestedDeclarative = null;
+  const t = new ImperativeTest('parent', test => {
+    t.endAfterSubtests();
+    nestedDeclarative = test.case('declarative', namespace, {
+      f1: [[1, 2], [2, 4], [3, 6]],
+    });
+  });
+  t.on('done', () => {
+    test.assert(t.success);
+    test.assert(t.results[0].success);
+    test.strictSame(t.results[0].type, 'subtest');
+    test.type(t.results[0].test, 'DeclarativeTest');
+    test.strictSame(t.results[0].test, nestedDeclarative);
+    test.end();
+  });
+});
+
+test('nested failing case', test => {
+  let nestedDeclarative = null;
+  const t = new ImperativeTest('parent', test => {
+    t.endAfterSubtests();
+    nestedDeclarative = test.case('declarative', namespace, {
+      f1: [[1, 1]],
+    });
+  });
+  t.on('done', () => {
+    test.assertNot(t.success);
+    test.assertNot(t.results[0].success);
+    test.strictSame(t.results[0].type, 'subtest');
+    test.type(t.results[0].test, 'DeclarativeTest');
+    test.strictSame(t.results[0].test, nestedDeclarative);
+    test.end();
+  });
+});
