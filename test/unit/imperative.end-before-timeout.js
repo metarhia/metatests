db0a98e99f6d6ac6d3c3e41080beb57da9a6884c
Denys Otrishko <shishugi@gmail.com>
2019-03-26 10:19:37 +0200

    Make imperative.end-before-timeout test more robust

--- test/unit/imperative.end-before-timeout.js
+++ test/unit/imperative.end-before-timeout.js
@@ -3,7 +3,7 @@
 const assert = require('assert');
 const { ImperativeTest } = require('../../metatests');
 
-const test = new ImperativeTest('', null, { timeout: 5 });
+const test = new ImperativeTest('', null, { timeout: 1000 });
 test.on('done', () => {
   assert.deepStrictEqual(test.results, []);
 });
