85f26443b1b9b94fa14c953efefa76c7cbff2626
Denys Otrishko <shishugi@gmail.com>
2019-04-23 14:24:04 +0300

    Implement test.resolves, test.rejects utilities
    These will allow to simplify interaction with multiple checks when
    using promises.
    
    PR-URL: https://github.com/metarhia/metatests/pull/189

---
+++ test/test-rejects-test.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const metatests = require('..');
+
+metatests.test('async test.reject test', test => {
+  const err = new Error('error');
+  const t = new metatests.ImperativeTest('Rejecting test', async () => {
+    const e1 = await t.rejects(Promise.reject(err), err);
+    t.strictSame(e1, err);
+    const e2 = await t.isRejected(Promise.reject(err), err);
+    t.strictSame(e2, err);
+  });
+  t.on('done', () => {
+    test.assert(t.success);
+    test.contains(t.results[0], {
+      type: 'isError',
+      actual: err,
+      expected: err,
+    });
+    test.contains(t.results[1], {
+      type: 'strictSame',
+      actual: err,
+      expected: err,
+    });
+    test.contains(t.results[2], {
+      type: 'isError',
+      actual: err,
+      expected: err,
+    });
+    test.contains(t.results[3], {
+      type: 'strictSame',
+      actual: err,
+      expected: err,
+    });
+    test.end();
+  });
+});
+
+metatests.test('test.reject test must throw resolve', test => {
+  const res = 42;
+  const t = new metatests.ImperativeTest('Rejecting test', async () => {
+    try {
+      await t.rejects(Promise.resolve(res), new Error());
+      t.fail('must throw on resolve');
+    } catch (e) {
+      t.strictSame(e, res);
+    }
+  });
+  t.on('done', () => {
+    test.assertNot(t.success);
+    test.contains(t.results[0], {
+      type: 'fail',
+      actual: res,
+      message: 'expected to be rejected, but was resolved',
+      success: false,
+    });
+    test.contains(t.results[1], {
+      type: 'strictSame',
+      actual: res,
+      expected: res,
+      success: true,
+    });
+    test.end();
+  });
+});
+
+metatests.test('test.reject subtest function', test => {
+  const err = new Error('error');
+  const t = new metatests.ImperativeTest('Rejecting test', () =>
+    t.rejects(() => Promise.reject(err), err)
+  );
+  t.on('done', () => {
+    test.assert(t.success);
+    test.contains(t.results[0], {
+      type: 'isError',
+      actual: err,
+      expected: err,
+    });
+    test.end();
+  });
+});
---
+++ test/test-resolves-test.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const metatests = require('..');
+
+metatests.test('async test.resolve subtest', test => {
+  const res = 42;
+  const t = new metatests.ImperativeTest('Resolving test', async () => {
+    const res1 = await t.resolves(Promise.resolve(res), res);
+    t.strictSame(res1, res);
+    const res2 = await t.isResolved(Promise.resolve(res), res);
+    t.strictSame(res2, res);
+  });
+  t.on('done', () => {
+    test.assert(t.success);
+    test.contains(t.results[0], {
+      type: 'strictSame',
+      actual: res,
+      expected: res,
+    });
+    test.contains(t.results[1], {
+      type: 'strictSame',
+      actual: res,
+      expected: res,
+    });
+    test.contains(t.results[2], {
+      type: 'strictSame',
+      actual: res,
+      expected: res,
+    });
+    test.contains(t.results[3], {
+      type: 'strictSame',
+      actual: res,
+      expected: res,
+    });
+    test.end();
+  });
+});
+
+metatests.test('test.resolve must throw on reject', test => {
+  const err = new Error('error');
+  const t = new metatests.ImperativeTest('Resolving test', async () => {
+    try {
+      await t.resolves(Promise.reject(err), 42);
+      t.fail('must throw on reject');
+    } catch (e) {
+      t.strictSame(e, err);
+    }
+  });
+  t.on('done', () => {
+    test.assertNot(t.success);
+    test.contains(t.results[0], {
+      type: 'fail',
+      actual: err,
+      message: 'expected to be resolved, but was rejected',
+      success: false,
+    });
+    test.contains(t.results[1], {
+      type: 'strictSame',
+      actual: err,
+      expected: err,
+      success: true,
+    });
+    test.end();
+  });
+});
+
+metatests.test('test.resolve subtest function', test => {
+  const res = 42;
+  const t = new metatests.ImperativeTest('Resolving test', () =>
+    t.resolves(() => Promise.resolve(res), res)
+  );
+  t.on('done', () => {
+    test.assert(t.success);
+    test.contains(t.results[0], {
+      type: 'strictSame',
+      actual: res,
+      expected: res,
+    });
+    test.end();
+  });
+});
