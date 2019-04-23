e4d82ad32fa1f9b89100f22eaa75dd0030a040fb
Denys Otrishko <shishugi@gmail.com>
2019-04-23 10:02:09 +0300

    Call test.end()/endAfterSubtests() on test promise resolve
    PR-URL: https://github.com/metarhia/metatests/pull/188

--- test/test-promise.js
+++ test/test-promise.js
@@ -16,3 +16,47 @@ metatests.test('must support promise reject', test => {
     test.end();
   });
 });
+
+metatests.test('must end on promise resolve', test => {
+  const t = new metatests.ImperativeTest('Resolve test', async () => {});
+  t.on('done', () => {
+    test.assert(t.success);
+    test.strictSame(t.results.length, 0);
+    test.end();
+  });
+});
+
+metatests.test('must not end on plan tests with promise', test => {
+  const t = new metatests.ImperativeTest('Resolve test', () => {
+    t.plan(1);
+    setTimeout(() => {
+      t.strictSame(42, 42);
+    }, 100);
+    return Promise.resolve();
+  });
+  t.on('done', () => {
+    test.assert(t.success);
+    test.end();
+  });
+});
+
+metatests.test('must endAfterSubtests on promise resolve', test => {
+  let subtest;
+  const t = new metatests.ImperativeTest('Resolve test', async () => {
+    subtest = t.test('Waiting subtest', t => {
+      setTimeout(() => t.end(), 100);
+    });
+  });
+  t.on('done', () => {
+    test.assert(t.success);
+    test.assert(subtest.done);
+    test.assert(subtest.success);
+    test.strictSame(t.results[0], {
+      type: 'subtest',
+      test: subtest,
+      message: subtest.caption,
+      success: subtest.success,
+    });
+    test.end();
+  });
+});
