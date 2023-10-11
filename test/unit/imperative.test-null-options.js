c344d4bfbf9fb4057e12244638b0525437ba28f1
Vladimir Myltsev <mylcev.vova@bk.ru>
2023-10-11 17:00:34 +0200

    fix: improve imperative test function to receive options as null (#268)
    PR-URL: https://github.com/metarhia/metatests/pull/268
    
    Co-authored-by: Timur Shemsedinov <timur.shemsedinov@gmail.com>

---
+++ test/unit/imperative.test-null-options.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const assert = require('assert');
+const { test } = require('../..');
+
+const subtest = test(
+  'should not failed when the passed options is null',
+  (test) => {
+    test.pass();
+    test.end();
+  },
+  null
+);
+assert.doesNotThrow(() => subtest);
